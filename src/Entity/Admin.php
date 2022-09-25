b22afff6f34854c4c078b98fbefe78b6086cc249
meguro <jmeguro18@gmail.com>
2022-09-25 11:47:19 +0900

    add toString to Admin entity

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -57,6 +57,11 @@ class Admin implements UserInterface, PasswordAuthenticatedUserInterface
         return (string) $this->username;
     }
 
+    public function __toString(): string
+    {
+        return $this->username;
+    }
+
     /**
      * @see UserInterface
      */
