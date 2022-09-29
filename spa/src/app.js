eb8aa06affbad1e2e7cf6a0505947af29dbd1c63
meguro <jmeguro18@gmail.com>
2022-09-29 13:16:35 +0900

    rewrite to router component

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,8 +1,23 @@
 import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+
+import Home from './pages/home';
+import Conference from './pages/conference';
 
 function App() {
     return (
-        <p>Hello World!</p>
+        <div>
+            <header>
+                <Link href="/">Home</Link>
+                <br />
+                <Link href="/conference/amsterdam2019">Amsterdam 2019</Link>
+            </header>
+
+            <Router>
+                <Home path="/" />
+                <Conference path="/conference/:slug" />
+            </Router>
+        </div>
     )
 }
 
